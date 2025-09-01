a82d298569167e2f07336cf092f950cfa73d5583
Naw San <nawsanbmo@gmail.com>
2025-09-02 03:48:51 +0900

    Initial commit

---
+++ src/components/Stats.js
@@ -0,0 +1,26 @@
+export function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding necessary items to your packing list🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedItems / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! ready to go 🧳"
+          : `You have ${
+              numItems === 1 ? "1 item" : `${numItems} items`
+            } on your list, and you already packed
+        ${packedItems} (${percentage}%)`}
+        .
+      </em>
+    </footer>
+  );
+}
